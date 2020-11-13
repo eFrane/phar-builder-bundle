e9a182d57b04397b281b14c66426c878b30fd715
eFrane <efrane@meanderingsoul.com>
2020-11-13 14:41:41 +0100

    Add tests to php-cs-fixer

---
+++ tests/TestCase.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @copyright 2020
+ * @author Stefan "eFrane" Graupner <stefan.graupner@gmail.com>
+ */
+
+namespace EFrane\PharBuilder\Tests;
+
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+
+}
