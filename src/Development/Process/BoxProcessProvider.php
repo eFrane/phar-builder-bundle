14f4d6a855a11c65dd252f0a9149121d1001acfc
Stefan Graupner <efrane@meanderingsoul.com>
2021-03-22 13:55:51 +0100

    fix: Revert class name refactoring

--- src/Development/Process/BoxProcessProvider.php
+++ src/Development/Process/BoxProcessProvider.php
@@ -5,7 +5,7 @@ namespace EFrane\PharBuilder\Development\Process;
 /**
  * Build a command for Box with the correct location.
  */
-final class BoxProcessProviderInterface extends AbstractProcessProvider implements ProcessProviderInterface
+final class BoxProcessProvider extends AbstractProcessProvider implements ProcessProviderInterface
 {
     protected function getToolCommand(): array
     {
