5278a8734104125d876e46968d9e6638586eae71
Stefan Graupner <efrane@meanderingsoul.com>
2021-03-23 10:28:22 +0100

    fix: Add return type hint

--- src/Bundle/PharBuilderBundle.php
+++ src/Bundle/PharBuilderBundle.php
@@ -25,7 +25,7 @@ class PharBuilderBundle extends Bundle
         $this->configureTwig();
     }
 
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         $container->registerForAutoconfiguration(IdentifiableProcessProviderInterface::class)
             ->addTag('phar_builder.process_provider');
